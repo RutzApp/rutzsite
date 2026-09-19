fbb197e0bc4bf80ee11fc47fa1cf49a9aa726122
Itay Naim <itaynaim@gmail.com>
2026-09-20 00:00:16 +0300

    deploy: site 2026-09-19T21:00Z

---
+++ _next/static/PKHSu4hOoZCCHN5nbamwh/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fguides\u002F[slug]","\u002Fpace-charts\u002F[distance]","\u002Ftraining\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/chunks/app/training/[slug]/page-be4ae1333e15bb76.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[521,409,575,283,301,384,125,200,44,920,321,515,767,2,723,663],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
